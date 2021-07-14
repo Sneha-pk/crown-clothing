7cc18a1e87af96df2f1937ea9e5eaaea744c6463
SnehaPk <snehap.k0507@gmail.com>
2021-07-14 16:19:57 +1000

    updating header, adding Shop and contact navigation and styling header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/Hompage.component';
-// /import Hats from './pages/Hats/Hats.component';
+import Header from './components/header/header.component';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 
@@ -9,6 +9,7 @@ function App() {
   return (
     <div>
       <Router>
+        <Header/>
         <Switch>
           <Route exact path='/' component={Homepage}/>
           <Route path='/shop' component={ShopPage}/>  
