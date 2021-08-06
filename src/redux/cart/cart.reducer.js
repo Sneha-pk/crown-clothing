97e9b7b571b9dd65bc6076bc7f7d32fae6d606e5
SnehaPk <snehap.k0507@gmail.com>
2021-08-06 13:38:43 +1000

    updated shop data with new data, updated cart utils to group item in cart reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,8 @@
-import {cartActionTypes} from './cart.type'
+import {cartActionTypes} from './cart.type';
+import { addItemToCart } from './cart.utils';
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems:[]
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,6 +12,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                     ...state,
                     hidden: !state.hidden
                 }
+        case cartActionTypes.ADD_ITEMS :
+            return{
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            }
          default:
              return state;
     }
